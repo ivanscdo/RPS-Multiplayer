aa89b61a552059f7c82e63520e682c91118554fb
Ivan <ivan.scdo@gmail.com>
2018-03-15 10:54:41 -0500

    pc tr315.10:53 v1.0 player objects

--- dev/dev.js
+++ dev/dev.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
 // console.log("red wine, success!")
 
+
+function pressStart () {
+
+    $("#player-login").on("click", function(){
+        event.preventDefault();
+
+        let database    = firebase.database(),
+            playerName  = $("#player-name").val().trim();
+        
+        database.ref("players/" + playerName);
+        database.ref("players/" + playerName + "/name/").set(playerName);
+        database.ref("players/" + playerName + "/wins/").set(0);
+        database.ref("players/" + playerName + "/losses/").set(0);
+        database.ref("players/" + playerName + "/ties/").set(0);
+        database.ref("players/" + playerName + "/choice/").set("");
+
+        $("#login-form").hide()
+
+        var welcomeScreen = $("<p>");
+        welcomeScreen.text("Player 1 " + playerName + ", ready!")
+        welcomeScreen.appendTo("#welcome-screen");
+
+    //END OF: $("#player-login").on("click", function(){
+    });
+
+//END OF: function pressStart () {
+}
+
+pressStart();
+
+//END OF: $(document).ready(function(){
 });
